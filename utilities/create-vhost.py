a55f4aed56c17178667a8612415e613563ccf7d4
hslavich <hernan.slavich@gmail.com>
2013-10-09 00:40:01 -0300

    Add simple script for create basic apache virtual host

---
+++ utilities/create-vhost.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+APACHE_PATH="/etc/apache2/sites-enabled/"
+
+def get_arg(arg, option):
+    try:
+        return sys.argv[arg]
+    except IndexError:
+        return raw_input('%s (Enter to empty): ' % option)
+
+def get_filename(server):
+    if server:
+        return APACHE_PATH + server
+        
+    return APACHE_PATH + 'new_host'
+
+def create_host(server, root):
+    f = open(get_filename(server), 'w')
+    f.write('<VirtualHost *:80>\n')
+    f.write('    ServerName %s\n' % server)
+    f.write('    DocumentRoot %s\n' % root)
+    f.write('</VirtualHost>\n')
+    return f
+
+f = create_host(get_arg(1, 'ServerName'), get_arg(2, 'DocumentRoot'))
+f.close()
+
+print(f.name)
+os.system('cat %s' % f.name)
